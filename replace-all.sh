e787ab3d5f1ace136d86a950c0308e8e7089ba4d
Sergei Eremenko <finalitik@gmail.com>
2019-01-09 15:37:20 +0200

    Improve replace-all.sh script #1

--- replace-all.sh
+++ replace-all.sh
@@ -1,66 +1,81 @@
-DIRECTORIES=("/home/$USER/.icons/Suru++" "/home/$USER/.local/share/icons/Suru++" "/usr/share/icons/Suru++" "/home/$USER/.icons/suru-plus" "/home/$USER/.local/share/icons/suru-plus" "/usr/share/icons/suru-plus" "/home/$USER/.icons/suru-plus-master" "/home/$USER/.local/share/icons/suru-plus-master" "/usr/share/icons/suru-plus-master")
+#!/usr/bin/env bash
 
-echo "Which type of colour do you prefer? Write the nummber"
-echo "1) solid 2) gradient"
-read type
+declare -i THEME_EXISTS=0
 
-if [ "$type" == "1" ]; then
-	echo "Write one of these colours to choose your favourite:"
-	echo "black gold green\n grey orange purple\n red yellow"
-	read color
-else
-	echo "Write one of these letters to choose your favourite gradient:"
-	echo "a) arrogrin b) telinkrin c) mycenean"
-	read color
-fi
+declare -a THEME_DIRS=(
+	"$HOME/.icons/Suru++"
+	"$HOME/.local/share/icons/Suru++"
+	"/usr/share/icons/Suru++"
+	"$HOME/.icons/suru-plus"
+	"$HOME/.local/share/icons/suru-plus"
+	"/usr/share/icons/suru-plus"
+	"$HOME/.icons/suru-plus-master"
+	"$HOME/.local/share/icons/suru-plus-master"
+	"/usr/share/icons/suru-plus-master"
+)
 
-go_dir_to_breeze()
-{
+declare -a CONTEXT_DIRS=(
+	"apps/16"
+	"places/16"
+	"mimetypes/16"
+)
 
-	change_colour()
-	{
-		for f in *svg; do
-   			case "$color" in
-    		a)
-		        perl arrogrin.pl "$f" > tmpFile && mv tmpFile "$f"
-		        ;;
-		    b)
-		        perl telinkrin.pl "$f" > tmpFile && mv tmpFile "$f"
-		        ;;
-		    c)
-		        perl mycenean.pl "$f" > tmpFile && mv tmpFile "$f"
-		        ;;
-		    black)
-		        perl black.pl "$f" > tmpFile && mv tmpFile "$f"
-		        ;;
-		    gold)
-		        perl gold.pl "$f" > tmpFile && mv tmpFile "$f"
-		        ;;
-		    green)
-		        perl green.pl "$f" > tmpFile && mv tmpFile "$f"
-		        ;;
-		    grey)
-		        perl grey.pl "$f" > tmpFile && mv tmpFile "$f"
-		        ;;
-		    orange)
-		        perl orangle.pl "$f" > tmpFile && mv tmpFile "$f"
-		        ;;
-		    purple)
-		        perl purple.pl "$f" > tmpFile && mv tmpFile "$f"
-		        ;;
-		    red)
-		        perl red.pl "$f" > tmpFile && mv tmpFile "$f"
-		        ;;
+declare -a COLORS=(
+	arrogrin
+	telinkrin
+	mycenean
+	black
+	gold
+	green
+	grey
+	orangle
+	purple
+	red
+)
+
+choose_color() {
+	local color
+	local PS3="(put a number)> "  # set custom prompt
+	local COLUMNS=1
+
+	echo "Choose one of these colours:"
+	select color in "${COLORS[@]}"; do
+		case "${color:-$REPLY}" in
+			"$color")
+				COLOR="$color"
+				break
+				;;
+			quit|[qQeE]*)
+				exit 0
+				;;
+			*) echo "invalid color -- '$REPLY'" >&2
 		esac
-		done
-	}
-
-	cd apps/16 && change_colour && cd ../../places/16 && change_colour && cd ../../mimetypes/16 && change_colour
-	
+	done < /dev/tty  # don't read from stdin
 }
 
-if [ -d $DIRECTORIES ]; then
-	go_dir_to_breeze
-else
-	echo "Icons theme Suru++ does not exist! You should rename the icons theme folder to “suru-plus” or “Suru++”."
+change_colour() {
+	perl "$COLOR".pl "$1" > "$1.tmp" && mv "$1.tmp" "$1"
+}
+
+
+
+for theme_dir in "${THEME_DIRS[@]}"; do
+	[ -d "$theme_dir" ] || continue
+	THEME_EXISTS=1
+
+	for context_dir in "${CONTEXT_DIRS[@]}"; do
+		for file_path in "$theme_dir/$context_dir/"*.svg; do
+			[ -f "$file_path" ] || continue  # is a file
+			[ -L "$file_path" ] && continue  # is not a symlink
+			[ -n "$COLOR" ] || choose_color
+			change_colour "$file_path"
+		done
+	done
+
+	exit 0
+done
+
+if [ "$THEME_EXISTS" -ne 1 ]; then
+	echo "Icons theme Suru++ does not exist! You should rename the icons theme folder to 'suru-plus' or 'Suru++'." >&2
+	exit 1
 fi
